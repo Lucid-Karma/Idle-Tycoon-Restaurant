9153682cb6906b07bccc081ac3aa94378116f656
Beyzanur Ozen <beyzaalways@gmail.com>
2025-04-10 20:32:31 +0300

    Flicker revised

--- Idle Restaurant/Assets/Project/[GAME]/Scripts/Objects/Furniture/NeonRandomFlicker.cs
+++ Idle Restaurant/Assets/Project/[GAME]/Scripts/Objects/Furniture/NeonRandomFlicker.cs
@@ -4,20 +4,16 @@ public class NeonRandomFlicker : MonoBehaviour
 {
     public Material neonMaterial;
     public Color baseColor = Color.cyan;
-    public float minTime = 1.5f;
-    public float maxTime = 10.5f;
+    public float minTime = 0.05f;
+    public float maxTime = 0.5f;
     public AudioSource flickerSound;
-    public AudioClip flickerClip;
 
     private float nextFlickerTime;
     private bool isOn = true;
 
-    private float[] spectrumData = new float[256]; // Frekans verisi için dizi
-
     void Start()
     {
         nextFlickerTime = Time.time + Random.Range(minTime, maxTime);
-        //flickerSound.Play();
     }
 
     void Update()
@@ -30,52 +26,11 @@ public class NeonRandomFlicker : MonoBehaviour
 
             if (isOn && flickerSound != null)
             {
-                PlayRandomFlickerSound();
+                flickerSound.Play();
             }
 
             nextFlickerTime = Time.time + Random.Range(minTime, maxTime);
         }
-
-        //// Sesin frekans verisini alýyoruz
-        //flickerSound.GetSpectrumData(spectrumData, 0, FFTWindow.BlackmanHarris);
-
-        //// Sesin frekans verilerine göre ýþýðý kontrol et
-        //float maxAmplitude = 0f;
-
-        //// Dalga verisinde en yüksek genliði buluyoruz
-        //for (int i = 0; i < spectrumData.Length; i++)
-        //{
-        //    if (spectrumData[i] > maxAmplitude)
-        //    {
-        //        maxAmplitude = spectrumData[i];
-        //    }
-        //}
-
-        //// Eðer genlik belli bir eþik deðerini aþarsa ýþýðý aç
-        //if (maxAmplitude > 0.1f) // Bu eþik deðeri sesin þiddetine göre ayarlanabilir
-        //{
-        //    UpdateNeonEmission(true);
-        //}
-        //else
-        //{
-        //    UpdateNeonEmission(false);
-        //}
-    }
-
-    void UpdateNeonEmission(bool state)
-    {
-        Color finalColor = state ? baseColor * Mathf.LinearToGammaSpace(2.0f) : Color.black;
-        neonMaterial.SetColor("_EmissionColor", finalColor);
-        nextFlickerTime = Time.time + Random.Range(minTime, maxTime);
-    }
-
-    void PlayRandomFlickerSound()
-    {
-        //AudioClip clip = flickerClips[Random.Range(0, flickerClips.Length)];
-        //flickerSound.pitch = Random.Range(1.0f, 1.5f);
-        //flickerSound.volume = Random.Range(0.1f, 0.7f);
-        //flickerSound.PlayOneShot(flickerClip);
-        flickerSound.Play();
     }
 
     private void OnDisable()
